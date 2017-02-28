611bc0792e3a7398a944f718a86380bc739d7ef5
MarkusBernhardt2 <markus.bernhardt@live.de>
2017-02-28 10:02:20 +0100

    test

---
+++ pokertests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy.Simple;
+
+namespace pokertests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BetRequestReturnsValue()
+        {
+
+            Assert.AreEqual(5,  PokerPlayer.BetRequest(null));
+
+
+        }
+    }
+}
--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -8,7 +8,6 @@ namespace Nancy.Simple
 
 		public static int BetRequest(JObject gameState)
 		{
-			//TODO: Use this method to return the value You want to bet
 			return 5;
 		}
 
